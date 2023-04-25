cab2563593c075d757dfbb10239201d3582ba3a8
liranmeir <liranmeir@gmail.com>
2023-04-25 10:21:40 +0300

    working version

---
+++ src/helpers.ts
@@ -0,0 +1,14 @@
+//TODO: implement
+//gets the events for the next 3 weeks
+function getAllDbEvents() {
+  //db.connect
+  //db.query({date:})
+  return [];
+}
+handlePushNotifications();
+function isEventShouldBeSent(event: never) {
+  throw new Error("Function not implemented.");
+}
+function sendPush(event: never) {
+  throw new Error("Function not implemented.");
+}
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+const usersWithEvents = [];
+/// ALL THIS SHOULD BE INSIDE LABMDA that will be triggered everyday
+//2. create usersWithEvents
+//3. for every usersWithEvents  send push with relavent events message
+
+//TODO: connection for db
+
+interface DBEvent {
+  id: number;
+  userSub: string;
+  title: string;
+}
+
+//TODO: implement
+function createEventJson(event: DBEvent) {
+  const APPLICATION_ID = "asdfasdfasdfadf";
+
+  return {
+    title: `${event.title} yada yad yad`,
+    token: "",
+    message: "",
+    service: "",
+  };
+}
+
+function handlePushNotifications() {
+  //1. query the db for all events
+  const dbEvents = getAllDbEvents();
+  dbEvents.forEach((event) => {
+    //const token = getTokenByUserid(event.userId)
+    // if (isEventShouldBeSent(event)) {
+    //   const eventJson = createEventJson(event, token);
+    //   sendPush(eventJson);
+    // }
+  });
+  //3. for every usersWithEvents  send push with relavent events message
+}
